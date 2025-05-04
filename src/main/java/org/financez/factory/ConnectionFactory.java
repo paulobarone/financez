14c74a7b71a5ad15fc5a08e91932366086db8933
Paulo <pmarquesbaronefilho18@protonmail.com>
2025-05-04 10:59:35 -0300

    test commit

--- src/main/java/org/financez/factory/ConnectionFactory.java
+++ src/main/java/org/financez/factory/ConnectionFactory.java
@@ -19,7 +19,6 @@ public class ConnectionFactory {
 
   public static Connection getConnection() throws SQLException {
     if (currentConnection == null || currentConnection.isClosed()) {
-      
       currentConnection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
       System.out.println(ANSI_GREEN + "---------- Nova conexão estabelecida ----------" + ANSI_RESET);
     }
