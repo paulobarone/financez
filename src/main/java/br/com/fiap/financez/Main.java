dcec30f7ed09073fe1cd9b1c9eee080024fd23a1
Paulo <pmarquesbaronefilho18@protonmail.com>
2025-05-25 14:03:30 -0300

    remove color imports

--- src/main/java/br/com/fiap/financez/Main.java
+++ src/main/java/br/com/fiap/financez/Main.java
@@ -4,9 +4,6 @@ import br.com.fiap.financez.dao.*;
 
 import java.sql.SQLException;
 
-import static br.com.fiap.financez.util.ColorsANSI.ANSI_CYAN;
-import static br.com.fiap.financez.util.ColorsANSI.ANSI_RESET;
-
 public class Main {
   public static void main(String[] args) throws SQLException {
     UserDao userDao = new UserDao();
